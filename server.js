1f757982de46d3709070bc8134d072a2f843dc0a
Nicolas <nickwil@users.noreply.github.com>
2019-11-24 17:13:05 -0500

    updated port

--- server.js
+++ server.js
@@ -279,7 +279,7 @@ function sendWebMention(source, target, webmentionEndpoint, callback){
 const express = require('express')
 var bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port =  process.env.PORT || 3000
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
